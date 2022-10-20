f573fd7dd3504aff7e62832e9da400832b2e483e
bekhzod <bexzod9834@gmail.com>
2022-10-20 11:25:14 +0900

    DiceeAppCommit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //IB - interface building outlet connected
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var upperLogoImage: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightRestartNumber = 0
+    var collection = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ]
+    var logoCollection = [ #imageLiteral(resourceName: "DiceeLogo"), #imageLiteral(resourceName: "Ghoster")]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       /* //who.what = value #imageLiteral(resourceName: "DiceSix")
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageViewOne.alpha = 0.5
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceOne")*/
+        
+        
     }
 
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        //diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
+        //diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+        upperLogoImage.image = logoCollection[Int.random(in: 0...1)]
+        
+        //diceImageViewOne.image = collection[leftDiceNumber]
+        diceImageViewOne.image = collection.randomElement()
+        print("left: \(collection.randomElement())")
+        leftDiceNumber += 1
+        if(leftDiceNumber == 6){
+            leftDiceNumber = 0
+        }
+        diceImageViewTwo.image = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ][rightRestartNumber]
+        rightRestartNumber = Int.random(in: 0...5)
+        print("right: \(rightRestartNumber)")
+        
+        if(rightRestartNumber >= 6){
+            rightRestartNumber = 0
+        }
+        
+        
+    }
+    /*
+    @IBAction func restartButtonPressed(_ sender: UIButton) {
+        //diceImageViewOne.image = #imageLiteral(resourceName: "DiceOne")
+        //diceImageViewTwo.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageViewOne.image = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ][rightRestartNumber - 1]
+        rightRestartNumber -= 1
+        if(rightRestartNumber == 1){
+            rightRestartNumber = 6
+        }
+    }*/
 }
 
